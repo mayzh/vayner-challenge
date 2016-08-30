039b2beaf834daf02bbcef3c4342b0f0db85b8c2
Dan Eversen <daneversen@gmail.com>
2016-08-30 13:28:00 -0400

    load script on document ready

--- vayner/public/js/main.js
+++ vayner/public/js/main.js
@@ -1,21 +1,24 @@
-console.log('script loaded')
+$(document).ready(function() {
 
-var root = 'http://jsonplaceholder.typicode.com';
+  console.log('script loaded')
+  var root = 'http://jsonplaceholder.typicode.com';
 
-$.ajax({
-  dataType: 'json',
-  url: root + '/users/1',
-  method: 'GET'
-}).then(function(data) {
-  console.log(data);
-  $(".user1").append(data.name);
-});
+  $.ajax({
+    dataType: 'json',
+    url: root + '/users/1',
+    method: 'GET'
+  }).then(function(data) {
+    console.log(data);
+    $(".user1").append(data.name);
+  });
 
-$.ajax({
-  dataType: 'json',
-  url: root + '/users/2',
-  method: 'GET'
-}).then(function(data) {
-  console.log(data);
-  $(".user2").append(data.name);
+  $.ajax({
+    dataType: 'json',
+    url: root + '/users/2',
+    method: 'GET'
+  }).then(function(data) {
+    console.log(data);
+    $(".user2").append(data.name);
+  });
+
 });
