d05e366e551c9204e9bf49f4e797d8bbf9d5d8bb
Dan Eversen <daneversen@gmail.com>
2016-08-30 12:49:16 -0400

    jquery linked, preliminary ajax call set up

--- vayner/public/js/main.js
+++ vayner/public/js/main.js
@@ -1,1 +1,10 @@
 console.log('script loaded')
+
+var root = 'http://jsonplaceholder.typicode.com';
+
+$.ajax({
+  url: root + '/posts/1',
+  method: 'GET'
+}).then(function(data) {
+  console.log(data);
+});
