1ab4a435f875442d316dfd44329eeb886207367c
Dan Eversen <daneversen@gmail.com>
2016-08-30 13:43:15 -0400

    append relevant albums to the table

--- vayner/public/js/main.js
+++ vayner/public/js/main.js
@@ -8,17 +8,40 @@ $(document).ready(function() {
     url: root + '/users/1',
     method: 'GET'
   }).then(function(data) {
-    console.log(data);
+   // console.log(data);
     $(".user1").append(data.name);
   });
+
+  $.ajax({
+    dataType: 'json',
+    url: root + '/albums?userId=1',
+    method: 'GET'
+  }).then(function(data) {
+   // console.log(data);
+    for(i = 0; i < data.length; i++){
+      $('.albums1').append(data[i].title + "<br>")
+    }
+  });
+
 
   $.ajax({
     dataType: 'json',
     url: root + '/users/2',
     method: 'GET'
   }).then(function(data) {
-    console.log(data);
+   // console.log(data);
     $(".user2").append(data.name);
+  });
+
+  $.ajax({
+    dataType: 'json',
+    url: root + '/albums?userId=2',
+    method: 'GET'
+  }).then(function(data) {
+   // console.log(data);
+    for(i = 0; i < data.length; i++){
+      $('.albums2').append(data[i].title + "<br>")
+    }
   });
 
 });
