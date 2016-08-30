874eb540468dfb00c07b0bd79a4374043a471a8c
Dan Eversen <daneversen@gmail.com>
2016-08-30 12:57:46 -0400

    got users

--- vayner/public/js/main.js
+++ vayner/public/js/main.js
@@ -3,7 +3,14 @@ console.log('script loaded')
 var root = 'http://jsonplaceholder.typicode.com';
 
 $.ajax({
-  url: root + '/posts/1',
+  url: root + '/users/1',
+  method: 'GET'
+}).then(function(data) {
+  console.log(data);
+});
+
+$.ajax({
+  url: root + '/users/2',
   method: 'GET'
 }).then(function(data) {
   console.log(data);
