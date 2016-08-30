201dcf2bc79d48cfeaa2c53841bd8b0412e778e3
Dan Eversen <daneversen@gmail.com>
2016-08-30 13:24:06 -0400

    append api responses to divs

--- vayner/public/js/main.js
+++ vayner/public/js/main.js
@@ -3,15 +3,19 @@ console.log('script loaded')
 var root = 'http://jsonplaceholder.typicode.com';
 
 $.ajax({
+  dataType: 'json',
   url: root + '/users/1',
   method: 'GET'
 }).then(function(data) {
   console.log(data);
+  $(".user1").append(data.name);
 });
 
 $.ajax({
+  dataType: 'json',
   url: root + '/users/2',
   method: 'GET'
 }).then(function(data) {
   console.log(data);
+  $(".user2").append(data.name);
 });
